7670fc3e778ec8c7e894920d8765ed8916b11dd5
saumykukreti <saumykukreti@gmail.com>
2018-02-04 10:42:35 +0530

    Minor changes

--- app/src/main/java/com/saumykukreti/learnforever/adapters/RevisePagerAdapter.java
+++ app/src/main/java/com/saumykukreti/learnforever/adapters/RevisePagerAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
@@ -51,13 +52,51 @@ public class RevisePagerAdapter extends PagerAdapter {
         View view = mLayoutInflater.inflate(R.layout.revise_item_layout, container, false);
 
         //Setting values
+        TextView titleTitleTextView = view.findViewById(R.id.text_note_title);
         TextView titleTextView = view.findViewById(R.id.text_title);
+        TextView contentInShortTitle = view.findViewById(R.id.text_content_in_short_title);
         TextView contentInShort = view.findViewById(R.id.text_content_in_short);
+        TextView categoryTitleTextView = view.findViewById(R.id.text_category_title);
+        TextView categoryTextView = view.findViewById(R.id.text_category);
+        TextView contentTitleTextView = view.findViewById(R.id.text_content_title);
         TextView contentTextView = view.findViewById(R.id.text_content);
+        ImageView titleLine = view.findViewById(R.id.image_note_title);
+        ImageView contentInShortLine = view.findViewById(R.id.image_note_cis);
+        ImageView categoryLine = view.findViewById(R.id.image_note_category);
+        ImageView contentLine = view.findViewById(R.id.image_note_content);
 
-        titleTextView.setText(note.getTitle());
-        contentInShort.setText(note.getContentInShort());
-        contentTextView.setText(note.getContent());
+        if(note.getTitle()!=null && !note.getTitle().isEmpty()) {
+            showViews(titleTitleTextView,titleTextView,titleLine);
+            titleTextView.setText(note.getTitle());
+        }
+        else{
+            hideViews(titleTitleTextView,titleTextView,titleLine);
+        }
+
+        if(note.getContentInShort()!=null && !note.getContentInShort().isEmpty()) {
+            showViews(contentInShort,contentInShortLine,contentInShortTitle);
+            contentInShort.setText(note.getContentInShort());
+        }
+        else{
+            hideViews(contentInShort,contentInShortLine,contentInShortTitle);
+        }
+
+        if(note.getCategory()!=null && !note.getCategory().isEmpty()) {
+            showViews(categoryTextView,categoryTitleTextView,categoryLine);
+            categoryTextView.setText(note.getCategory());
+        }
+        else{
+            hideViews(categoryTextView,categoryTitleTextView,categoryLine);
+        }
+
+         if(note.getContent()!=null && !note.getContent().isEmpty()) {
+            showViews(contentTextView,contentTitleTextView,contentLine);
+             contentTextView.setText(note.getContent());
+        }
+        else{
+            hideViews(contentTextView,contentTitleTextView,contentLine);
+        }
+
         container.addView(view);
         return view;
     }
@@ -70,5 +109,17 @@ public class RevisePagerAdapter extends PagerAdapter {
 
     public interface RevisePagerAdapterListener{
         void noteClicked();
+    }
+
+    private void hideViews(View viewOne, View viewTwo, View viewThree){
+        viewOne.setVisibility(View.GONE);
+        viewTwo.setVisibility(View.GONE);
+        viewThree.setVisibility(View.GONE);
+    }
+
+    private void showViews(View viewOne, View viewTwo, View viewThree){
+        viewOne.setVisibility(View.VISIBLE);
+        viewTwo.setVisibility(View.VISIBLE);
+        viewThree.setVisibility(View.VISIBLE);
     }
 }
